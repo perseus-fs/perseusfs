2297c64b023ad72a7b35f09dd936b1d9abc8ecf6
Diogo Martino <diogo.martino10@gmail.com>
2025-04-12 16:05:56 +0100

    fix: hide logs IP's in demo mode

--- server/src/routes/request-logs/list.ts
+++ server/src/routes/request-logs/list.ts
@@ -1,11 +1,24 @@
+import { SettingKey } from '@perseusfs/shared';
 import { RequestLog } from '../../database/models/request-log';
+import { Settings } from '../../database/models/settings';
 import type { TCustomRequest, TRes } from '../../types';
 
 const listRequestLogs = async (req: TCustomRequest, res: TRes) => {
   const logs = await RequestLog.findAll();
 
+  const demoMode = Settings.get(SettingKey.DEMO_MODE);
+  const isSuperUser = req.user?.id === 1;
+  const isDemoLocked = demoMode && !isSuperUser;
+
+  const targetLogs = isDemoLocked
+    ? logs.map((log) => ({
+        ...log,
+        address: 'hidden'
+      }))
+    : logs;
+
   return res({
-    logs
+    logs: targetLogs
   });
 };
 
